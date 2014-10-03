84113de3ee93b9c378a327494008cbcd91924ba9
Felix Ableitner <me@nutomic.com>
2014-10-03 13:24:27 +0300

    Code cleanup, moved Fragment into seperate file, added basic layout.

---
+++ app/src/main/java/com/nutomic/ensichat/activities/MainActivity.java
@@ -0,0 +1,17 @@
+package com.nutomic.ensichat.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.nutomic.ensichat.R;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
